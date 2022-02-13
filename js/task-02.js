fb3779e3375fef0521096da02983b04e7a05145b
EVGENIY-12 <pet3094@gmail.com>
2022-02-13 15:33:14 +0200

    Add TASK-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const makeItem = (itemName) => {
+
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = itemName;
+
+  return itemEl;
+};
+
+const arrayItems = ingredients.map(makeItem)
+
+listEl.append(...arrayItems)
+console.log( listEl);
