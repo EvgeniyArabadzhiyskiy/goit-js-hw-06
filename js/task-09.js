591d86c9c30f03c6018c8c1e2091b177b12e759e
EVGENIY-12 <pet3094@gmail.com>
2022-02-18 19:08:19 +0200

    Add TASK-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const refs = {
+  btnChangeColor: document.querySelector('.change-color'),
+  labelColorName: document.querySelector('.color'),
+};
+
+refs.btnChangeColor.addEventListener('click', onChengeBodyColor);
+
+function onChengeBodyColor(event) {
+  refs.labelColorName.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = refs.labelColorName.textContent;
+
+};
